56334102cde3ab0eb228f2f72051b3431d028736
Benjamin Calmerman <ruby.calmerman@gmail.com>
2024-11-26 15:45:13 +0100

    Thrust implemented ship & UI

---
+++ Assets/Scripts/Dashboard.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dashboard : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    private PlayerMovement playerMovement;
+
+    [SerializeField] private Slider thrust;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerMovement = player.GetComponent<PlayerMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        thrust.value = playerMovement.Thrust;
+        Debug.Log(playerMovement.Thrust);
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField][Range(0,1)] public float Thrust; // { get; private set; }
+    [SerializeField] public float MaxThrust; // { get; private set; }
+    [SerializeField] public Vector3 Velocity; // { get; private set; }
+
+    [SerializeField] private float drag = 1;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,11 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Velocity += transform.forward * Time.deltaTime * Thrust;
+        transform.position += Velocity * Time.deltaTime;
+
+        Velocity *= 1 - drag * Time.deltaTime;
+
+        if (Velocity.magnitude < 0.0001f) Velocity = new Vector3(0,0,0);
     }
 }
