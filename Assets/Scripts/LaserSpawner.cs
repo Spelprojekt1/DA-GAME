1a180233d785fb858ea2cd82fbd327614c3064e2
Ville Lundin <153563435+Clown40@users.noreply.github.com>
2024-11-29 17:39:57 +0100

    ruby tvinagde mig

--- Assets/Scripts/LaserSpawner.cs
+++ Assets/Scripts/LaserSpawner.cs
@@ -1,5 +1,6 @@
 using UnityEditor;
 using UnityEngine;
+using Vector3 = System.Numerics.Vector3;
 
 public class LaserSpawner : ProjectileSpawner
 {
@@ -8,8 +9,16 @@ public class LaserSpawner : ProjectileSpawner
     [SerializeField] private float startCooldown = 0.2f;
     private float cooldown = 0f;
     private bool active = false;
+   
+ 
 
     [SerializeField] private GameObject laser;
+    private float laserRayDistance = 0f;
+    //Skapar laserRay som används i CastRay metod.
+    Ray laserRay;
+
+    
+   // public float laserRayDistance = laser.transform.localScale 
 
     // Start is called before the first frame update
     void Start()
@@ -20,10 +29,14 @@ public class LaserSpawner : ProjectileSpawner
     // Update is called once per frame
     void Update()
     {
+        
+        //Timer tickar ned när duration större än 0
         if (duration > 0)
         {
+            
             duration -= Time.deltaTime;
         }
+       //
         else
         {
             if (active)
@@ -42,11 +55,40 @@ public class LaserSpawner : ProjectileSpawner
     
     public override void Fire()
     {
+        //Aktiverar laserRay
+        Castray();
         if (!active && cooldown <= 0)
         {
+           
+           
+     
             laser.SetActive(true);
             active = true;
             duration = startDuration;
         }
+    }
+
+    void Castray()
+    {
+        this.laserRay = new Ray(transform.position, transform.forward * laser.transform.localScale.z);
+        CheckForColliders();
+    }
+    void CheckForColliders()
+    {
+        //träffar laser något object med någon Physics component händer x 
+        if (Physics.Raycast(this.laserRay, out RaycastHit hit))
+        {
+            //Debug.Log(gameObject)
+        }
+       // RaycastHit hit;
+        //Vector3 laserRayDisctance = Vector3.Zero;
+        //Skapar en storleken av ray som är lika lång som laserRayDistance.
+      //  Vector3 laserRay = transform.position;
+      //  //Ritar ut en Röd laserRay rakt fram.
+       // Debug.DrawRay(laserRay, transform.forward *laserRayDistance , Color.red);
+        //Om laser ray träffar x  så händer x
+        //if (Physics.Raycast(laserRay, transform.forward, out hit, laserRayDistance))
+       // {
+            
     }
 }
