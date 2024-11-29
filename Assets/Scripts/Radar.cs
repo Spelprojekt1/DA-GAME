efa0bee5176fcdd3228647b5f094067a95aa2e52
Benjamin Calmerman <ruby.calmerman@gmail.com>
2024-11-29 17:37:03 +0100

    Radar exists in cockpit

--- Assets/Scripts/Radar.cs
+++ Assets/Scripts/Radar.cs
@@ -8,31 +8,22 @@ using UnityEngine;
 public class Radar : MonoBehaviour
 {
     [SerializeField] private Transform origin;
-    [SerializeField] private float MAX_DISTANCE = 200.0f;
-    private const float bezierP1 = 0.0f;
-    [SerializeField] private float bezierP2 = 0.8f;
-    private const float bezierP3 = 1f;
-    private float bezierT = 0f;
     [SerializeField] private GameObject RadarPing;
-    [SerializeField] private List<GameObject> enemies = new List<GameObject>();
-    [SerializeField] private List<GameObject> radarEnemies = new List<GameObject>();
     public void Ping()
     {
         // Remove all children
-        //for (int i = 0; i < transform.childCount; i++)
-        //{
-        //    DestroyImmediate(transform.GetChild(i).gameObject);
-        //}
-        //enemies.Clear();
-        //radarEnemies.Clear();
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            DestroyImmediate(transform.GetChild(i).gameObject);
+        }
 
         // Get all objects with the tag "RadarEnemy"
-        //enemies = GameObject.FindGameObjectsWithTag("RadarEnemy").ToList();
-        for (int i = 0; i < enemies.Count; i++)
+        foreach (GameObject o in GameObject.FindGameObjectsWithTag("RadarEnemy"))
         {
             GameObject ping = (GameObject)PrefabUtility.InstantiatePrefab(RadarPing);
+            ping.GetComponent<RadarPing>().target = o;
+            ping.GetComponent<RadarPing>().origin = origin;
             ping.transform.SetParent(transform);
-            radarEnemies.Add(ping);
         }
     }
 
@@ -45,43 +36,6 @@ public class Radar : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Ping();
-        if (enemies.Count == 0) return;
-        if (radarEnemies.Count == 0) return;
-
-        Vector3 originPosition = origin.position;
-        for (int i = 0; i < enemies.Count; i++)
-        {
-            Vector3 target = enemies[i].transform.position;
-            Debug.Log(target);
-            Debug.Log(Mathf.Abs((target - originPosition).magnitude) < MAX_DISTANCE);
-            if (Mathf.Abs((target - originPosition).magnitude) < MAX_DISTANCE)
-            {
-                // Set ping to active
-                radarEnemies[i].SetActive(true);
-
-                // Calculate bezierT
-                bezierT = (target - originPosition).magnitude / MAX_DISTANCE;
-
-                // Calculate one dimensional quadratic bezier curve
-                float a = (1 - bezierT) * bezierP1 + bezierT * bezierP2;
-                float b = (1 - bezierT) * bezierP2 + bezierT * bezierP3;
-                float c = (1 - bezierT) * a + bezierT * b;
-
-                // Use bezier curve to scale the ping's distance on the radar
-                Vector3 rPingVector = (target - originPosition).normalized * c;
-
-                // Rotate rPingVector opposite to origin's rotation
-                rPingVector = Quaternion.Inverse(origin.transform.rotation) * rPingVector;
-
-                // Move ping to rPingVector and account for radar scaling
-                radarEnemies[i].GetComponent<RadarPing>().UpdatePing(Vector3.Scale(rPingVector, transform.lossyScale));
-            }
-            else
-            {
-                // Set ping to not active
-                radarEnemies[i].SetActive(false);
-            }
-        }
+        
     }
 }
--- Assets/Scripts/RadarPing.cs
+++ Assets/Scripts/RadarPing.cs
@@ -1,20 +1,72 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using System.Security.Cryptography;
 using UnityEngine;
 
+[Serializable]
+struct QBezier
+{
+    public float P1;
+    public float P2;
+    public float P3;
+    public float T;
+    public QBezier(float p1, float p2, float p3, float t)
+    {
+        P1 = p1;
+        P2 = p2;
+        P3 = p3;
+        T = t;
+    }
+}
 public class RadarPing : MonoBehaviour
 {
+    [SerializeField] private float MAX_DISTANCE = 200.0f;
+    [SerializeField] private QBezier bezier = new(0f, 0.8f, 1f, 0f);
     
+    public Transform origin;
+    public GameObject target;
+    
+    [SerializeField] private GameObject XZ;
     [SerializeField] private Transform ping;
     [SerializeField] private Transform positiveY;
     [SerializeField] private Transform negativeY;
     // Update is called once per frame
-    public void UpdatePing(Vector3 target)
+    void Update()
     {
-        transform.localPosition = new Vector3(target.x, 0, target.z);
-        ping.transform.localPosition = new Vector3(0, target.y, 0);
+        
+        // transform.localPosition = new Vector3(target.x, 0, target.z);
+        // ping.transform.localPosition = new Vector3(0, target.y, 0);
+        //
+        // positiveY.transform.localScale = new Vector3(1, Mathf.Max(0,target.z), 1);
+        // negativeY.transform.localScale = new Vector3(1, Mathf.Max(0,-target.z), 1);
+        
+        Vector3 targetPos = target.transform.position;
+        Vector3 originPos = origin.position;
+        if (Mathf.Abs((targetPos - originPos).magnitude) < MAX_DISTANCE)
+        {
+            // Set ping to active
+            XZ.SetActive(true);
 
-        positiveY.transform.localScale = new Vector3(1, Mathf.Max(0,target.z), 1);
-        negativeY.transform.localScale = new Vector3(1, Mathf.Max(0,-target.z), 1);
+            // Calculate bezierT
+            bezier.T = (targetPos - originPos).magnitude / MAX_DISTANCE;
+
+            // Calculate one dimensional quadratic bezier curve
+            float a = (1 - bezier.T) * bezier.P1 + bezier.T * bezier.P2;
+            float b = (1 - bezier.T) * bezier.P2 + bezier.T * bezier.P3;
+            float c = (1 - bezier.T) * a + bezier.T * b;
+
+            // Use bezier curve to scale the ping's distance on the radar
+            Vector3 rPingVector = (targetPos - originPos).normalized * c;
+
+            // Rotate rPingVector opposite to origin's rotation
+            rPingVector = Quaternion.Inverse(origin.transform.rotation) * rPingVector;
+
+            // Move ping to rPingVector and account for radar scaling
+            transform.localPosition = Vector3.Scale(rPingVector, transform.lossyScale);
+        }
+        else
+        {
+            // Set ping to not active
+            XZ.SetActive(false);
+        }
     }
 }
