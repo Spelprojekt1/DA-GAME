57173f032f6fba9385c3067d44c74dc2b6961b3e
Benjamin Calmerman <ruby.calmerman@gmail.com>
2024-12-03 14:32:43 +0100

    Player now has health and shield

--- Assets/Scripts/Cockpit/CockpitBehaviour.cs
+++ Assets/Scripts/Cockpit/CockpitBehaviour.cs
@@ -5,15 +5,17 @@ public class CockpitBehaviour : MonoBehaviour
 {
     [SerializeField] private GameObject player;
     private PlayerMovement playerMovement;
-
+    private PlayerBehaviour playerBehaviour;
     [SerializeField] private Slider thrust;
     [SerializeField] private Slider reverseThrust;
-
+    [SerializeField] private Slider health;
+    [SerializeField] private Slider shield;
     [SerializeField] private Image pointer;
     // Start is called before the first frame update
     void Start()
     {
-        playerMovement = player.GetComponent<PlayerMovement>();
+    playerMovement = player.GetComponent<PlayerMovement>();
+    playerBehaviour = player.GetComponent<PlayerBehaviour>();
     }
 
     // Update is called once per frame
@@ -21,6 +23,9 @@ public class CockpitBehaviour : MonoBehaviour
     {
         thrust.value = playerMovement.Thrust;
         reverseThrust.value = -playerMovement.Thrust;
+        health.value = playerBehaviour.Health / playerBehaviour.MaxHealth;
+        shield.value = playerBehaviour.Shield / playerBehaviour.MaxShield;
+        
         transform.rotation = player.transform.rotation;
         pointer.rectTransform.localPosition = new Vector3(
             playerMovement.Rotation.z * -200,
--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -7,6 +7,17 @@ public class PlayerBehaviour : MonoBehaviour
 {
     private Transform target;
     private ProjectileSpawner[] primaryWeapons;
+    [SerializeField] private float health;
+    [SerializeField] private float maxHealth;
+    [SerializeField] private float shield;
+    [SerializeField] private float maxShield;
+    [Tooltip("shield per second that's regenerated")]
+    [SerializeField] private float shieldRegen;
+    public float Health => health;
+    public float MaxHealth => maxHealth;
+    public float Shield => shield;
+    public float MaxShield => maxShield;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +28,8 @@ public class PlayerBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (shield > maxShield) shield = maxShield;
+        if (shield < maxShield) shield += shieldRegen * Time.deltaTime;
     }
     public void OnPrimary()
     {
