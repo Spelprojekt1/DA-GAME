982d9bc4da28590141d57983694bfae37c94d4aa
volibjorn <c24liner@student.his.se>
2024-12-04 10:31:18 +0100

    fixed application handler in start menu

--- Assets/Scripts/ApplicationHandlers.cs
+++ Assets/Scripts/ApplicationHandlers.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 // This component can be used to access methods for changing the scene, and quitting the game
 // It can be used together with UnityEventOnTrigger, or UI-button-events, to decide when a scene should be changed or the game should be closed
-public class ApplicationHandler : MonoBehaviour
+public class ApplicationHandlers : MonoBehaviour
 {
     public void ChangeScene()
     {
