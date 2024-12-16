c6c003f2027ba16635ba99f1837e7e62665ebc09
Ville Lundin <153563435+Clown40@users.noreply.github.com>
2024-12-16 16:23:26 +0100

    pilla lite på patrol state scripten

--- Assets/Scripts/ENEMY SCRIPTS/EnemyTestStates/Patrol.cs
+++ Assets/Scripts/ENEMY SCRIPTS/EnemyTestStates/Patrol.cs
@@ -2,7 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//public class Patrol : BaseState
-//{
-    
-//}
+public class Patrol : BaseState
+{
+    public override void Enter()
+    {
+       base.Enter();
+    }
+    public override void UpdateLogic()
+    {
+        base.UpdateLogic();
+    }
+}
